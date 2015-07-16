9eb6304721d6187cf65e1b175d1c53e43626e59b
Nathan Beyer <nbeyer@gmail.com>
2015-07-16 15:37:14 -0500

    Stub out loading a file of quotes

--- qotd-server.go
+++ qotd-server.go
@@ -1,26 +1,45 @@
 package main
 
-import "net"
+import "bufio"
 import "fmt"
+import "net"
+import "os"
+
+func checkError(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
 
 func main() {
   l, err := net.Listen("tcp", ":17")
-  if err != nil {
-    panic(err)
+  checkError(err)
+  defer l.Close()
+  fmt.Println("port connected")
+
+  f, err := os.Open("quotes")
+  checkError(err)
+  defer f.Close()
+
+  var quotes []string
+  quotes = make([]string, 0, 5)
+  s := bufio.NewScanner(bufio.NewReader(f))
+
+  for s.Scan() {
+    quotes = append(quotes, s.Text())
   }
+  fmt.Println("quotes loaded; count =", len(quotes))
 
   fmt.Println("server started")
-
-  defer l.Close()
 
   for {
     c, err := l.Accept()
     if err != nil {
       panic(err)
     }
-    fmt.Println("Connection made ...")
-    c.Write([]byte("Insert witty quote here."))
+    fmt.Println("connection made ...")
+    c.Write([]byte(quotes[0]))
     c.Close()
-    fmt.Println("Connection closed ...")
+    fmt.Println("connection closed ...")
   }
 }
